0629233d75318adc565fe510a73428372a563ade
jpeck40684 <jeannettepeck@gmail.com>
2017-08-09 18:12:23 -0400

    added links and tags

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,8 @@ class TweetsController < ApplicationController
 
   before_action :authenticate_user!
 
+  include TweetsHelper
+
   # GET /tweets
   # GET /tweets.json
   def index
@@ -26,7 +28,10 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = Tweet.create(tweet_params)
+    @tweet = get_tagged(@tweet)
+
+    # @tweet = Tweet.new(tweet_params)
 
     respond_to do |format|
       if @tweet.save
@@ -37,6 +42,7 @@ class TweetsController < ApplicationController
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /tweets/1
@@ -71,6 +77,6 @@ class TweetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:message, :user_id)
+      params.require(:tweet).permit(:message, :user_id, :link)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'tweets#index'
+  root 'epicenter#feed'
+
+  get 'show_user' => 'epicenter#show_user'
+
+  get  'now_following' => 'epicenter#now_following'
+
+  get 'unfollow' => 'epicenter#unfollow'
+
+  get 'tag_tweets' => 'epicenter#tag_tweets'
 
   resources :tweets
   devise_for :users
